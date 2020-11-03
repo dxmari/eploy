80f112747c931d5a9c95adb4af5071e5a579e60b
dxmari <dxmari007@gmail.com>
2020-11-03 13:59:15 +0530

    git func added

--- helpers/server.helper.ts
+++ helpers/server.helper.ts
@@ -15,11 +15,11 @@ class ServerHelper {
     }
 
     async navigateToAppPathAndLaunchScript(cloudConfig: CloudConfig, ws: ExtWebSocket) {
-        
-        ws.send("\n☢Redirect to " + cloudConfig.application_path + "\n\n☢Update the files from git repo(" + cloudConfig.ref + ")\n\n☢Run pre launch scripts " + cloudConfig.pre_launch_script + "\n\n");
+
+        ws.send("\n1)Redirect to " + cloudConfig.application_path + "\n\n2)Update the files from git repo(" + cloudConfig.ref + ")\n\n3)Run pre launch scripts " + cloudConfig.pre_launch_script + "\n\n");
         ws.send('start_spinner');
         var logs = await execShell(`
-                cd ${cloudConfig.application_path} && ${'git pull ' + cloudConfig.ref} && ${cloudConfig.pre_launch_script}
+                cd ${cloudConfig.application_path} ${cloudConfig.ref ? (' &&  git pull ' + cloudConfig.ref.replace('/', ' ')) : ''} && ${cloudConfig.pre_launch_script}
         `);
         ws.send(logs)
         ws.send('stop_spinner')
