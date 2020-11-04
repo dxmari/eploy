40c28b68d2112568ceea41cd2488cfaf5fed5bf0
dxmari <dxmari007@gmail.com>
2020-11-05 00:28:26 +0530

    start-daemon changes && ubuntu

--- lib/start-daemon.d.ts
+++ lib/start-daemon.d.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 declare const _default: () => Promise<void>;
 export default _default;
 //# sourceMappingURL=start-daemon.d.ts.map
--- start-daemon.ts
+++ start-daemon.ts
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import WebSocketServer from './server/index'
 
 export default async () =>{
